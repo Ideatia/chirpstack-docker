73983b73ddf0c4f138bcc273c0fc67fd875013d9
郝甲辰 <haojiachen@ut.cn>
2024-01-12 13:13:17 +0800

    修复数据库初始化

--- configuration/postgresql/initdb/003-init-sensor.sh
+++ configuration/postgresql/initdb/003-init-sensor.sh
@@ -2,5 +2,5 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create database sensor-monitor with owner postgres;
+    create database "sensor-monitor" with owner postgres;
 EOSQL
